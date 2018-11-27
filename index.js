7c2adce7fe44030c5067c8459b5f1a607c8f0fe9
Christoph Friedrich <christoph@friedrich-whv.de>
2018-11-27 20:07:47 +0100

    Expose ALL Vue components
    I needed DescriptionElement and LinkList for the Hub and thought: Well, the rest may be helpful somewhere too, so why not export the whole lot?

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ module.exports = {
     DocGenTOC: require('./src/components/DocGenTOC.vue').default,
     DocGenLinks: require('./src/components/DocGenLinks.vue').default,
     DocGenProcesses: require('./src/components/DocGenProcesses.vue').default,
-    ProcessPanel: require('./src/components/ProcessPanel.vue').default
+    
+    DescriptionElement: require('./src/components/DescriptionElement.vue').default,
+    LinkList: require('./src/components/LinkList.vue').default,
+    ProcessExampleElement: require('./src/components/ProcessExampleElement.vue').default,
+    ProcessPanel: require('./src/components/ProcessPanel.vue').default,
+    SchemaElement: require('./src/components/SchemaElement.vue').default,
+    SchemaPanel: require('./src/components/SchemaPanel.vue').default
 }
